bcac50bc9cba7962ec102a80a8ecfebcd21549a6
Arbaaz <arbaaz8158@gmail.com>
2026-09-26 13:19:07 +0530

    Python OOP.

---
+++ 10_OOP/01_simple_class.py
@@ -0,0 +1,12 @@
+class Chai: # Class definition
+    pass
+
+class ChaiTime():
+    pass
+
+print(type(Chai))
+
+ginger_tea = Chai() # Object of class Chai
+print(type(ginger_tea))
+print(type(ginger_tea) is Chai)
+print(type(ginger_tea) is ChaiTime)
